2a4e0ab87455ddac615c002d296ae100e3cf5210
Yuriy Rymarchuk <yura.ita.com@gmail.com>
2019-03-10 14:11:26 +0100

    First Commit, stable console version

---
+++ client.py
@@ -0,0 +1,45 @@
+import socket
+from threading import *
+import time
+import os
+import sys
+
+#Clean the console screen (Windows = cls / Linux = clear)
+def clear():
+	os.system('cls' if os.name == 'nt' else 'clear')
+
+def newName():
+	name = raw_input('Please enter your name--> ')
+	return name
+
+def chatThread(client_socket):
+	while True:
+		message = client_socket.recv(100)
+		print(message)
+
+
+#Set the connection variables
+ip = '192.168.1.3'
+#ip = raw_input("Enter the server ip--> ")
+port = 1234
+
+#Create socket object
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#Connect to the server
+client_socket.connect( (ip,port) )
+client_socket.sendall(newName())
+
+clear()
+#Start receiving data from server (Other clients)
+Thread(target = chatThread, args = (client_socket,)).start()
+
+#Send the data to the server
+while True:
+	message = raw_input()
+	sys.stdout.write('\x1b[1A') #CURSOR_UP_ONE
+	sys.stdout.write('\x1b[2K') #ERASE_LINE
+	client_socket.sendall(message)
+	if message == 'exit()':
+		client_socket.close()
+		break
---
+++ server.py
@@ -0,0 +1,51 @@
+import socket
+from threading import *
+import time
+
+#Vector of clients
+clients = []
+
+#Set the connection variables
+ip = socket.gethostbyname_ex(socket.gethostname())[2][1]
+port = 1234
+
+#Server object
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#Bind the selected port to this process
+server_socket.bind( (ip,port) )
+
+#Accept packets
+server_socket.listen(25)
+print("Server is running...")
+
+#Send message to all clients except the sender
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+#Manage of data that comes to the server
+def serviClient(client_socket):
+    name = client_socket.recv(100)
+    broadcast(r"<< %s entered the chat room! >>" % (name))
+    #print("<< %s entered the chat room! >>" % (name))
+    while True:
+        message = client_socket.recv(100)
+        if message == 'exit()':
+            clients.remove(client_socket)
+            broadcast(r"<< %s left the chat room! >>" % (name))
+            #print("<< %s left the chat room! >>" % (name))
+            break
+        else:
+            broadcast(r"%s: %s" % (name, message))
+            #print("%s: %s" % (name, message))
+    time.sleep(1)
+    client_socket.close()
+
+#Accepting new client
+while True:
+    client_socket,client_ip = server_socket.accept()
+    if client_socket:
+        clients.append(client_socket)
+        print(clients)
+        Thread(target = serviClient, args = (client_socket,)).start()
