b8b282bbbe5579e7d6c570daef723e2c2fc18f01
Yuriy Rymarchuk <45283261+DropB1t@users.noreply.github.com>
2019-03-11 09:30:19 +0100

    Detect local ip new method

--- server.py
+++ server.py
@@ -5,8 +5,21 @@ import time
 #Vector of clients
 clients = []
 
+#Detect an local ip adress
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
 #Set the connection variables
-ip = socket.gethostbyname_ex(socket.gethostname())[2][0]
+ip = get_ip()
 port = 1234
 
 #Server object
